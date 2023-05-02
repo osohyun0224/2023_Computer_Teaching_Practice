08f8a48f2274a92f328c91ed93fed92b3b2341fe
osohyun0224 <osohyun0224@naver.com>
2023-05-02 14:43:45 +0900

    feat: 01단원 mission sloved

---
+++ topic01/01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+  int x,y;
+  x=5;
+  y=2;
+  printf("%d\n",x/y);
+  printf("%d\n",x%y);
+  return 0;
+}
---
+++ topic01/01.java
@@ -0,0 +1,10 @@
+public class test{
+  public static void main(String args[]){
+    int x,y;
+    x=5;
+    y=2;
+    System.out.printf("%d\n", x/y);
+    System.out.printf("%d\n", x%y);
+    
+  }
+}
